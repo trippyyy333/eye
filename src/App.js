6988a7b095946bf144a69bb2d39c1509b5f69f8c
trippyyy333 <75495653+trippyyy333@users.noreply.github.com>
2023-05-26 21:16:13 -0400

    a
    sa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <><div>Asddsapp</div><header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header></>
   )
 }
 
